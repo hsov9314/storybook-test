6191e1ebbf8583b9a596298affa986da6992cf0f
hsov9314 <hsov3149@gmail.com>
2023-02-26 00:46:49 +0900

    [fix] set default color to Button

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ type Props = {
   onClick: () => void
 }
 
-export const Button = ({ label, color, onClick }: Props) => {
+export const Button = ({ label, color = 'bg-blue-500', onClick }: Props) => {
   return (
     <button onClick={onClick} className={`rounded-md h-10 w-20 font-sans font-medium ${color}`}>
       {label}
