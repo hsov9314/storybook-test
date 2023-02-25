eaf13ba05d7bc40a2135bca1d78acc290a06834e
hsov9314 <hsov3149@gmail.com>
2023-02-26 00:46:59 +0900

    [fix] fix button imports

--- app/src/features/Counter/Counter.tsx
+++ app/src/features/Counter/Counter.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button } from '../../stories/Button'
+import { Button } from '../../components/Button/Button'
 
 export const Counter = () => {
   const [count, setCount] = useState(0)
@@ -8,8 +8,8 @@ export const Counter = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <p className="text-3xl">{count}</p>
       <div className="flex">
-        <Button onClick={() => setCount(count - 1)} label="-"></Button>
-        <Button onClick={() => setCount(count + 1)} label="+"></Button>
+        <Button onClick={() => setCount(count - 1)} label="-" color="bg-blue-500"></Button>
+        <Button onClick={() => setCount(count + 1)} label="+" color="bg-pink-500"></Button>
       </div>
     </div>
   )
