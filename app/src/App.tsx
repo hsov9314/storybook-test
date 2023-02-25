6528d861eaec71e10623c44024806e53e618358e
hsov9314 <hsov3149@gmail.com>
2023-02-25 22:20:17 +0900

    [fix] fix main content

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,10 @@
 import { Card } from './components/Card/Card'
 import { MainLayout } from './components/Layout/MainLayout'
-import { Summary } from './features/summary'
 
 function App() {
   return (
     <>
-      <MainLayout>
-        <Summary />
-      </MainLayout>
+      <MainLayout>Main Content</MainLayout>
     </>
   )
 }
