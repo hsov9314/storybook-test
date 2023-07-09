4a7946f283722c8435df4b0f15d2b7371bc97dff
hsov9314 <hsov3149@gmail.com>
2023-07-10 08:24:27 +0900

    feat: :lipstick: add styles to sample components

--- app/src/features/JestTest/components/Pages/Pages.tsx
+++ app/src/features/JestTest/components/Pages/Pages.tsx
@@ -2,8 +2,13 @@ import { Link } from 'react-router-dom'
 
 export const HomePage = () => (
   <div>
-    <h1>Welcome to Home Page</h1>
-    <Link to="/about">Go to About Page</Link>
+    <h1 className="mb-5">Welcome to Home Page</h1>
+    <Link
+      to="/about"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    >
+      Go to About Page
+    </Link>
   </div>
 )
 
